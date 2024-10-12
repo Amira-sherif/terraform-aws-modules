1b94c85bb5ddf5445333703ee2ec1d9a97dd2005
Amira-sherif <amirasherifkamel@gmail.com>
2024-10-12 08:04:08 +0300

    Update providor.tf

--- providor.tf
+++ providor.tf
@@ -2,11 +2,11 @@
 provider "aws" {
   region = "us-east-1"
 }
-terraform {
-  backend "s3" {
-    bucket = "mybucket"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = " DynamoDB-name"
-  }
-}
+//terraform {
+//  backend "s3" {
+//    bucket = "mybucket"
+  //  key    = "terraform.tfstate"
+ //   region = "us-east-1"
+  //  dynamodb_table = " DynamoDB-name"
+ // }
+// }
