6a9bcfb864b52790b5df1848fed9b018e4c32ceb
Amira-sherif <amirasherifkamel@gmail.com>
2024-10-13 17:30:39 +0300

    Update providor.tf

--- providor.tf
+++ providor.tf
@@ -3,13 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-provider "aws" {
-  region = "us-east-1"
-}
 // terraform {
 //  backend "s3" {
 //    bucket = "mybucket"
